56e1ba09973730105a6daddb0dc100b2887e98b6
mkutaGD <mkuta@griddynamics.com>
2023-02-14 13:53:53 +0100

    All tests now works

--- unitTestingTask.test.js
+++ unitTestingTask.test.js
@@ -1,17 +1,34 @@
 const unitTestingTask = require("./unitTestingTask");
 
-test('format argument is not a string', () => {
+test('throws exception if `format` argument is not a string', () => {
     const f = () => { unitTestingTask(99, new Date()) }
     expect(f).toThrow('Argument `format` must be a string');
 })
 
-test('date argument is not a date', () => {
+test('throws exception if `date` argument is not a date', () => {
     const f = () => { unitTestingTask('MM', true) }
     expect(f).toThrow('Argument `date` must be instance of Date or Unix Timestamp or ISODate String');
 })
 
-test('change language with no arguments', () => {
-    expect(unitTestingTask.lang()).toBe('en')
+
+
+test('register date format', () => {
+    expect(unitTestingTask.register('ISODateTimeTZ', 'YYYY-MM-ddThh:mm:ssZ')(new Date('2023-2-13'))).toBe('2023-02-13T12:00:00+01:00')
+})
+
+test('unitTestingTask with format which already exists', () => {
+    expect(unitTestingTask('ISODateTimeTZ', new Date('2023-2-13'))).toBe('2023-02-13T12:00:00+01:00')
+})
+
+test('unitTestingTask with format which not exist', () => {
+    unitTestingTask.lang('pl', undefined);
+    console.log(unitTestingTask._languages)
+    console.log(unitTestingTask('DD-MM-YYYY', new Date()))
+})
+
+test('formats by default in English if language is not set', () => {
+    unitTestingTask.lang()
+    expect(unitTestingTask._languages.en._months[0]).toBe('January')
 })
 
 test('change language with no options', () => {
@@ -27,18 +44,4 @@ test('change language with options', () => {
     unitTestingTask.lang('pl', options);
 
     expect(unitTestingTask._languages.pl.months[0]).toBe('styczeń');
-})
-
-test('register date format', () => {
-    expect(unitTestingTask.register('ISODateTimeTZ', 'YYYY-MM-ddThh:mm:ssZ')(new Date('2023-2-13'))).toBe('2023-02-13T12:00:00+01:00')
-})
-
-test('unitTestingTask with format which already exists', () => {
-    expect(unitTestingTask('ISODateTimeTZ', new Date('2023-2-13'))).toBe('2023-02-13T12:00:00+01:00')
-})
-
-test('unitTestingTask with format which not exist', () => {
-    unitTestingTask.lang('pl');
-    console.log(unitTestingTask._languages)
-    console.log(unitTestingTask('DD-MM-YYYY', new Date()))
 })
