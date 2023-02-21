730318599ec5935c12f87c48d77d29cc786875e9
mkutaGD <mkuta@griddynamics.com>
2023-02-21 12:08:42 +0100

    Added some tests

--- unitTestingTask.test.js
+++ unitTestingTask.test.js
@@ -21,8 +21,25 @@ describe('Main function', () => {
 
     test('unitTestingTask with format which not exist', () => {
         unitTestingTask.lang('pl', undefined);
-       
-        expect(unitTestingTask('DD-MM-YYYY', new Date('2023-02-15'))).toBe('śr-02-2023')
+
+        expect(unitTestingTask('DD-MM-YYYY', new Date('2023-02-15'))).toBe('śr-02-2023');
+        expect(unitTestingTask('DDD-MMMM-YY', new Date('2023-02-15'))).toBe('środa-luty-23')
+        expect(unitTestingTask('DDD-MMM-YY', new Date('2023-02-15'))).toBe('środa-lut-23')
+        expect(unitTestingTask('D-M-YY', new Date('2023-02-15'))).toBe('Śr-2-23')
+        expect(unitTestingTask('dd-M-YY', new Date('2023-02-15'))).toBe('15-2-23')
+        expect(unitTestingTask('d-M-YY', new Date('2023-02-15'))).toBe('15-2-23')
+        expect(unitTestingTask('d-M-YY HH:mm:ss', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34')
+        expect(unitTestingTask('d-M-YY HH:mm:ss:ff', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34:000')
+        expect(unitTestingTask('d-M-YY HH:mm:ss:f', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34:0')
+        expect(unitTestingTask('d-M-YY H:m:s', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34')
+        expect(unitTestingTask('d-M-YY hh:mm:ss', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 03:34:34')
+        expect(unitTestingTask('d-M-YY h:m:s', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 3:34:34')
+
+        unitTestingTask.lang('en');
+
+        expect(unitTestingTask('d-M-YY HH:mm:ss A', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34 PM')
+        expect(unitTestingTask('d-M-YY HH:mm:ss a', new Date('2023-02-15 15:34:34'))).toBe('15-2-23 15:34:34 pm')
+
     })
 
     test('noConflict function', () => {
